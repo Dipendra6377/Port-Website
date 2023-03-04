709994e836a627144353182365165cc2bb2cbf28
unknown <singhdipendra6377@gmail.com>
2023-03-04 18:01:06 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -9,3 +9,9 @@ app.use(cookieParser());
 
 import { userRouter } from "./routes/User.js";
 app.use("/api/v1", userRouter);
+
+app.use(express.static(path.resolve("./frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("./frontend/build/index.html"));
+});
